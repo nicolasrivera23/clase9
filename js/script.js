6175470e1dd23bf3f0dd4eb6627a268a444ed4fb
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2025-10-23 20:34:50 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+console.log("Hola JS desde un archivo externo");
+
+// Comentario de una sola línea
+
+/*
+Comentario
+Multilínea
+1
+2
+*/
+
+/*Declaración de variables*/
+
+let operando1 = 15;
+let operando2 = 20;
+
+let $variable = 3;
+let _variable = "Hola";
+
+let caseSensitive;
+let CaseSensitive;
+
+/*CamelCase*/
+let palabraCompuestaTres;
+
+
+
+let total = operando1 + operando2;
+
+console.log(total);
+
