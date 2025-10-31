6a08f4559c4e6bc51f0a7b4991c46ded22b786f0
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2025-10-30 21:05:56 -0300

    Se agregaron ejercicios

--- js/script.js
+++ js/script.js
@@ -29,3 +29,47 @@ let total = operando1 + operando2;
 
 console.log(total);
 
+let numero = 21;
+numero = 33;
+
+const IVA = 21;
+//IVA = 10.5;
+console.log(IVA);
+
+/*Number*/
+let entero = 22;
+let decimal = 6.66;
+const PI = 3.1416;
+
+/*Boolean*/
+let esMayor = true;
+
+/* String o cadenas de texto */
+let cadenaTexto = "Esto es un texto";
+
+/*Método parseInt / parseFloat*/
+
+let valorString = "33";
+
+let valorParseado = parseInt(valorString);
+console.log(typeof valorString);
+
+console.log(valorParseado);
+
+
+let numero1 = 11;
+
+console.log(valorParseado + numero1);
+
+let valorStringFloat = "22.2";
+console.log(typeof valorStringFloat);
+
+let valorDecimal = parseFloat(valorStringFloat);
+console.log(typeof valorDecimal);
+
+
+
+
+
+
+
